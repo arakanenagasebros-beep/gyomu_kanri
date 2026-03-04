53ee1abd02b1f19d7f56e1845f4e0434298f9d55
arakanenagasebros-beep <arakane.nagasebros@gmail.com>
2026-03-04 23:25:02 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// 環境差分はこのファイルだけ編集
+// ※GitHub Pages 等で公開する場合、ここに書いたURLは閲覧可能になります。
+//   Apps Script 側の公開範囲/トークン等で保護してください。
+window.APP_CONFIG = {
+  DEFAULT_API_URL: "https://script.google.com/macros/s/XXXX/exec"
+};
