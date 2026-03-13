043d108521f0700ddf7acdb4e0645845ac30b112
arakanenagasebros-beep <arakane.nagasebros@gmail.com>
2026-03-13 22:43:41 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 /* config.js - 業務管理アプリ設定 */
 window.APP_CONFIG = {
-  DEFAULT_API_URL: ""  // GAS Web App URL をここに設定（例: "https://script.google.com/macros/s/xxxxx/exec"）
+  DEFAULT_API_URL: "https://script.google.com/macros/s/AKfycbybMaVCIMh5SeWVx7IkIVXrYv7VHB0rBZBvINF1Cq5E6ak0szIMdFcaFQNuhVvb-Fk7/exec"  // GAS Web App URL をここに設定（例: "https://script.google.com/macros/s/xxxxx/exec"）
 };
