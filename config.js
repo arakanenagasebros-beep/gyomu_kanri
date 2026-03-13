2fdf8941c23f3a369413b17be2ed0fcdafd1099f
arakanenagasebros-beep <arakane.nagasebros@gmail.com>
2026-03-13 17:01:33 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // ※GitHub Pages 等で公開する場合、ここに書いたURLは閲覧可能になります。
 //   Apps Script 側の公開範囲/トークン等で保護してください。
 window.APP_CONFIG = {
-  DEFAULT_API_URL: "https://script.google.com/macros/s/AKfycbyvqrFJlGX4zSGU7WRU2FsjltNQ0GC2Iu84EKOg841qkfaNkr2Y7xat0qonx2BsWA14/exec"
+  DEFAULT_API_URL: "https://script.google.com/macros/s/AKfycbx1qqQqZe59Zwp2zI1K7T-gYMpeX8uyunHpyKhFH05wH2KuMGeYHgWihgBI5QhbSygc/exec"
 };
