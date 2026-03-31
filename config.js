402ae37254ea92907959058ad01c6625879c2a69
arakanenagasebros-beep <arakane.nagasebros@gmail.com>
2026-03-31 20:53:59 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 /* config.js - 業務管理アプリ設定 */
 window.APP_CONFIG = {
-  DEFAULT_API_URL: "https://script.google.com/macros/s/AKfycbzLAelW-Y7B67T-bqrsz7L_Az1soSaFMd8ni-rNWEkVgXuUBI1591qmUkA-iL9wEnoN/exec"）
+  DEFAULT_API_URL: "https://script.google.com/macros/s/AKfycbwcLAj2xMSCpP2CBn_Y_lLUCJdtBuGizGXQXoGKiuXoNpihypWuoDUwWuvOFsODex9s/exec"）
 };
